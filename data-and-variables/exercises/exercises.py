ef8d541159bf3f42eef3483b883e50b072700a07
MyWorld 2.0 <myworld2.0@MyWorlds-MacBook-Air.local>
2025-11-05 00:55:09 -0600

    exercises.py

--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -1,20 +1,26 @@
 # 1. Declare and assign the variables here:
 shuttle_name = "Determination"
 shuttle_speed_mph = 17500
-kilometers_to_mars_km = 225000000
+distance_to_mars_km = 225000000
 distance_to_moon_km = 384400
 miles_per_km = 0.621
 
 # 2. Use print() to print the 'type' each variable. Print one item per line.
 print(type(shuttle_name))
 print(type(shuttle_speed_mph))
-print(type(kilometers_to_mars_km))
+print(type(distance_to_mars_km))
 print(type(distance_to_moon_km))
 print(type(miles_per_km))
+# Answer: <class 'str'>
+#<class 'int'>
+#<class 'int'>
+#<class 'int'>
+#<class 'float'>
 
 # Code your solution to exercises 3 and 4 here:
 # Step 1: Convert km to miles
-miles_to_mars = kilometers_to_mars_km * miles_per_km
+miles_to_mars = distance_to_mars_km * miles_per_km
+
 
 # Step 2: Calculate hours to Mars
 hours_to_mars = miles_to_mars / shuttle_speed_mph
@@ -24,6 +30,7 @@ days_to_mars = hours_to_mars / 24
 
 # Step 4: Print result
 print(shuttle_name, "will take", round(days_to_mars), "days to reach Mars.")
+#Answer: Determination will take 333 days to reach Mars.
 
 # Code your solution to exercise 5 here
 kilometers_to_moon = 384400
@@ -34,3 +41,4 @@ hours_to_moon = miles_to_moon / shuttle_speed_mph
 days_to_moon = hours_to_moon / 24
 
 print(shuttle_name + " will take " + str(round(days_to_moon, 2)) + " days to reach the Moon.")
+#Answer: Determination will take 0.57 days to reach the Moon.
