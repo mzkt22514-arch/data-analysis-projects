6b7495f39865aec9f54d3c966581434f3d84e030
MyWorld 2.0 <myworld2.0@MyWorlds-MacBook-Air.local>
2025-11-05 20:53:30 -0600

    exercises01.py

--- booleans-and-conditionals/exercises01.py
+++ booleans-and-conditionals/exercises01.py
@@ -1,9 +1,13 @@
 # Assign the variables for exercise 1 here:
-
-
+engine_indicator_light = "red blinking"
+space_suits_on = True
+shuttle_cabin_ready = True
+crew_status = space_suits_on and shuttle_cabin_ready
+computer_status_code = 200
+shuttle_speed = 15000
 
 # BEFORE running the code, predict what will be printed to the console by the following statements:
-
+# My prediction is that it'll be Engines are Off
 if engine_indicator_light == "green": 
   print("engines have started")
 elif engine_indicator_light == "green blinking": 
